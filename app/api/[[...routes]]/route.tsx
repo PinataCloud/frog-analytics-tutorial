ab7bf3ef29ea54524b8c3bf79bb28716d672edd8
Steve <stevedsimkins@gmail.com>
2024-02-29 14:36:32 -0700

    init

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,67 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from "frog";
+import { handle } from "frog/vercel";
+import { PinataFDK } from "pinata-fdk";
+
+type State = {
+  count: number;
+};
+
+const pinataFdk = new PinataFDK({
+  pinata_jwt: process.env.PINATA_JWT as string,
+  pinata_gateway: process.env.GATEWAY_URL as string,
+});
+
+const app = new Frog<State>({
+  basePath: "/api",
+  initialState: {
+    count: 0,
+  },
+  // hubApiUrl: "https://hub.pinata.cloud"
+});
+
+app.use("/", async (context: any, next) => {
+  await pinataFdk.analyticsMiddleware(context, "frog-analytics-tutorial", next)
+});
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+app.frame("/", (c) => {
+  const { buttonValue, deriveState } = c;
+  const state = deriveState((previousState) => {
+    if (buttonValue === "inc") previousState.count++;
+    if (buttonValue === "dec") previousState.count--;
+  });
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "linear-gradient(to right, #6FD392, #83E1C8)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+          fontSize: "64px",
+        }}
+      >
+        <div style={{
+          fontSize: "136px"
+        }}>🐸</div>
+        Ribbits: {state.count}
+      </div>
+    ),
+    intents: [
+      <Button value="dec">Less Ribbits</Button>,
+      <Button value="inc">More Ribbits</Button>,
+    ],
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
